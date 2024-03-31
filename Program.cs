2b2b9723d806a3e9f92488a55fdd4d8d5c6450cd
Alexander Markov <foenix89@mail.ru>
2024-03-31 21:27:50 +0300

    решиди 2 задачу

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*На основе символов строки (тип string) сформир массив
+символов (тип char[]). Вывести массив на экран.
+
+Указание
+Метод строки ToCharArray() не использовать.
+Пример
+“Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
+20
+*/
+
+static char[] StringToCharArray(string str)
+{
+    char[] array = new char[str.Length];
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        array[i] = str[i];
+    }
+
+    return array;
+}
+
+System.Console.WriteLine(StringToCharArray("Hello!"));
